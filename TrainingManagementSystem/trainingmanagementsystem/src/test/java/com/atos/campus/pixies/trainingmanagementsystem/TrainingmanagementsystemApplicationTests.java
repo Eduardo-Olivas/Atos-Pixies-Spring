356eab74e15bcc417e800f0f2ab41ea7333d944c
Ulises Lopez - A838598 <victor-ulises.lopez@atos.net>
2021-11-25 13:51:00 -0600

    Created project from Spring Initializr

---
+++ TrainingManagementSystem/trainingmanagementsystem/src/test/java/com/atos/campus/pixies/trainingmanagementsystem/TrainingmanagementsystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atos.campus.pixies.trainingmanagementsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrainingmanagementsystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
