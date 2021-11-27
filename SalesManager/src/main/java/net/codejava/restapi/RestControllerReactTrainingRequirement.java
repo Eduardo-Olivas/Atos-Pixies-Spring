36609aa8382beffb1526422a2ed102b57ad71d33
Jose Luis Matias Pineda <jose-luis.matias@atos.net>
2021-11-27 12:51:26 -0600

    UPDATE: Just added an API for everyone to consume.

--- SalesManager/src/main/java/net/codejava/restapi/RestControllerReactTrainingRequirement.java
+++ SalesManager/src/main/java/net/codejava/restapi/RestControllerReactTrainingRequirement.java
@@ -4,9 +4,11 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import net.codejava.dao.TrainingRequirementMasterDAO;
+import net.codejava.model.TrainingExecutionMaster;
 import net.codejava.model.TrainingRequirementMaster;
 
 @RestController
@@ -20,5 +22,12 @@ public class RestControllerReactTrainingRequirement {
 		List<TrainingRequirementMaster>  list = dao.list();
 		
 		return list;
+	}
+	
+	@GetMapping("/api/TrainingRequirements/{id}")
+	public TrainingRequirementMaster getOneTrainingRequirement(@PathVariable String id) {
+		TrainingRequirementMaster training = dao.get(id);
+		
+		return training;
 	}
 }
