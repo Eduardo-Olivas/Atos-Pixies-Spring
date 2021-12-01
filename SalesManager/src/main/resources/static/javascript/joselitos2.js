4cae4c4f6b0afe13f20a8050e340a416124d26c6
Jose Luis Matias Pineda <jose-luis.matias@atos.net>
2021-12-01 17:27:15 -0600

    UPDATE: Better imposible

---
+++ SalesManager/src/main/resources/static/javascript/joselitos2.js
@@ -0,0 +1,35 @@
+$(document).ready( function(){
+    $('#execution-id-url').hide();
+    let parameter = $('#execution-id-url').text();
+
+    const apiExecution = async (param) => {
+        data = await fetch("http://localhost:8085/api/TrainingExecutionMaster/" + param);
+        parsed = await data.json();
+        console.log(parsed);
+        return parsed;
+    }
+    
+    const apiCallOne = async (parameter) => {
+		
+        retrievedData = await fetch("http://localhost:8085/api/TrainingRequirements/" + parameter);
+        data = await retrievedData.json();
+        $("#id-api").text(data.requirementID);
+        $("#date-api").text(data.requirementReceivedDate);
+        $("#user-api").text(data.requirementUser);
+        $("#vertical-api").text(data.requirementUserVertical);
+        $("#total-api").text(data.totalCandidates);
+        $("#days-api").text(data.totalDurationDays);
+        $("#area-api").text(data.trainingArea);
+        $("#description-api").text(data.trainingDescription);
+        $("#timezone-api").text(data.trainingTimeZone);
+        
+        return data;
+    }
+
+    async function OneToRule() {
+            let data = await apiExecution(parameter);
+            apiCallOne(data.requirementID);
+    }
+
+    OneToRule();
+})
