cfcabaf9562467feb54e82f5684449050810dcbf
Ulises Lopez - A838598 <victor-ulises.lopez@atos.net>
2021-12-02 17:42:21 -0600

    Fixed AppController_NavCommon missing

--- TrainingManagementSystem/trainingmanagementsystem/src/main/java/com/atos/campus/pixies/trainingmanagementsystem/controller/AppController_NavCommon.java
+++ TrainingManagementSystem/trainingmanagementsystem/src/main/java/com/atos/campus/pixies/trainingmanagementsystem/controller/AppController_NavCommon.java
@@ -7,11 +7,58 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class AppController_NavCommon {
-	
 
-	@RequestMapping("/NavCommon")
-	public String viewHomePage() {
-	    return "NavCommon";
+	@RequestMapping("/layout")
+	public String viewLayout() {
+	    return "layout";
+	    return "main_layout";
 	}
-	
+
+	@RequestMapping("/IndexLBP")
+	public String IndexLBP() {
+	    return "View/IndexLBP";
+	}
+
+	@RequestMapping("/IndexTrainer")
+	public String IndexTrainer() {
+	    return "View/IndexTrainer";
+	}
+
+	@RequestMapping("/InProcessTraining")
+	public String InProcessTraining() {
+	    return "View/InProcessTraining";
+	}
+
+	@RequestMapping("/Login")
+	public String Login() {
+	    return "View/Login";
+	}
+
+	@RequestMapping("/NewRequirement")
+	public String NewRequirement() {
+	    return "View/NewRequirement";
+	}
+
+	@RequestMapping("/RejectedLBP")
+	public String RejectedLBP() {
+	    return "View/RejectedLBP";
+	}
+
+	@RequestMapping("/RequirementConfirmed")
+	public String RequirementConfirmed() {
+	    return "View/RequirementConfirmed";
+	}
+
+	@RequestMapping("/TrainerAcceptance")
+	public String TrainerAcceptance() {
+	    return "View/TrainerAcceptance";
+	}
+
+	@RequestMapping("/TrainningRequest")
+	public String TrainningRequest() {
+	    return "View/TrainningRequest";
+	}
+
+}
+
 }
