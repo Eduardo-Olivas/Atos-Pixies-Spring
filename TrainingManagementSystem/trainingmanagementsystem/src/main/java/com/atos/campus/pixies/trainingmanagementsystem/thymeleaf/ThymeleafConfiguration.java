cb13f118e46d40347cfec9c418e24fde52580492
Ulises Lopez - A838598 <victor-ulises.lopez@atos.net>
2021-12-02 12:31:24 -0600

    Working fragments

---
+++ TrainingManagementSystem/trainingmanagementsystem/src/main/java/com/atos/campus/pixies/trainingmanagementsystem/thymeleaf/ThymeleafConfiguration.java
@@ -0,0 +1,21 @@
+package com.atos.campus.pixies.trainingmanagementsystem.thymeleaf;
+
+import org.springframework.context.annotation.Configuration;
+import org.thymeleaf.spring5.SpringTemplateEngine;
+
+import nz.net.ultraq.thymeleaf.layoutdialect.LayoutDialect;
+import nz.net.ultraq.thymeleaf.layoutdialect.decorators.strategies.GroupingStrategy;
+
+/**
+ * ThymeleafConfig
+ */
+@Configuration
+ public class ThymeleafConfiguration {
+    
+    public SpringTemplateEngine thymeleafLayoutEngine() {
+        SpringTemplateEngine engine = new SpringTemplateEngine();
+        engine.addDialect(new LayoutDialect(new GroupingStrategy()));
+        return engine;
+    }
+    
+}
