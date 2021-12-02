c051170f53c9c7b13f49a18a966d32c6fad58a3f
Ulises Lopez - A838598 <victor-ulises.lopez@atos.net>
2021-12-02 14:21:51 -0600

    Deledet old css

--- TrainingManagementSystem/trainingmanagementsystem/src/main/java/com/atos/campus/pixies/trainingmanagementsystem/controller/AppControllerPublic.java
+++ TrainingManagementSystem/trainingmanagementsystem/src/main/java/com/atos/campus/pixies/trainingmanagementsystem/controller/AppControllerPublic.java
@@ -3,8 +3,6 @@ package com.atos.campus.pixies.trainingmanagementsystem.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-
-
 @Controller
 public class AppControllerPublic {
 
@@ -15,6 +13,52 @@ public class AppControllerPublic {
 
 	@RequestMapping("/layout")
 	public String viewLayout() {
-	    return "layout";
+	    return "main_layout";
 	}
+
+	@RequestMapping("/IndexLBP")
+	public String IndexLBP() {
+	    return "View/IndexLBP";
+	}
+
+	@RequestMapping("/IndexTrainer")
+	public String IndexTrainer() {
+	    return "View/IndexTrainer";
+	}
+
+	@RequestMapping("/InProcessTraining")
+	public String InProcessTraining() {
+	    return "View/InProcessTraining";
+	}
+
+	@RequestMapping("/Login")
+	public String Login() {
+	    return "View/Login";
+	}
+
+	@RequestMapping("/NewRequirement")
+	public String NewRequirement() {
+	    return "View/NewRequirement";
+	}
+
+	@RequestMapping("/RejectedLBP")
+	public String RejectedLBP() {
+	    return "View/RejectedLBP";
+	}
+
+	@RequestMapping("/RequirementConfirmed")
+	public String RequirementConfirmed() {
+	    return "View/RequirementConfirmed";
+	}
+
+	@RequestMapping("/TrainerAcceptance")
+	public String TrainerAcceptance() {
+	    return "View/TrainerAcceptance";
+	}
+
+	@RequestMapping("/TrainningRequest")
+	public String TrainningRequest() {
+	    return "View/TrainningRequest";
+	}
+	
 }
